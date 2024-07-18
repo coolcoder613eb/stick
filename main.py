32897175c662130ca43b8cf94c8801d197ac7a97
coolcoder613eb <96163908+coolcoder613eb@users.noreply.github.com>
2024-07-18 15:25:30 +1000

    Added more ops, improved error reporting, and executed test correctly

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from typing import Dict, List
 
 test_code = """PUSH 8
+; Doubling routine
 BEGIN DUB
 PUSH 2
 MUL
 END
 DUP
 PUSH 2
+; Modulo
 MOD
 PUSH 0
 CMP
 IF DUB
 SHOW
- ; TEST
-ECHO Hello, World!
 """
 
 
@@ -35,7 +35,9 @@ class Interpreter:
                         self.def_stack.pop()
                         return
                     else:
-                        raise Exception("Incorrect number of arguments for END!")
+                        raise Exception(
+                            f"{len(toks)}: Incorrect number of arguments for END!"
+                        )
             self.defs[self.def_stack[-1]].append(line)
         elif len(self.def_stack) == 0:
             if toks[0].upper() == "PUSH":
@@ -44,7 +46,111 @@ class Interpreter:
                     print("Push:", value)
                     self.int_stack.append(value)
                 else:
-                    raise Exception("Incorrect number of arguments for PUSH!")
+                    raise Exception(
+                        f"{len(toks)}: Incorrect number of arguments for PUSH!"
+                    )
+            elif toks[0].upper() == "SHOW":
+                if len(toks) == 1:
+                    print("Show")
+                    print(self.int_stack.pop())
+                else:
+                    raise Exception(
+                        f"{len(toks)}: Incorrect number of arguments for SHOW!"
+                    )
+            elif toks[0].upper() == "IF":
+                if len(toks) == 2:
+                    name = toks[1]
+                    if name in self.defs:
+                        print("If:", name)
+                        if self.int_stack.pop():
+                            for def_line in self.defs[name]:
+                                self.run(def_line)
+                    else:
+                        raise Exception(f"{name}: Unknown Label!")
+                else:
+                    raise Exception(
+                        f"{len(toks)}: Incorrect number of arguments for BEGIN!"
+                    )
+            elif toks[0].upper() == "DUP":
+                if len(toks) == 1:
+                    print("Dup")
+                    self.int_stack.append(self.int_stack[-1])
+                else:
+                    raise Exception(
+                        f"{len(toks)}: Incorrect number of arguments for DUP!"
+                    )
+            elif toks[0].upper() == "ADD":
+                if len(toks) == 1:
+                    print("Add")
+                    self.int_stack.append(self.int_stack.pop(-2) + self.int_stack.pop())
+                else:
+                    raise Exception(
+                        f"{len(toks)}: Incorrect number of arguments for ADD!"
+                    )
+            elif toks[0].upper() == "SUB":
+                if len(toks) == 1:
+                    print("Sub")
+                    self.int_stack.append(self.int_stack.pop(-2) - self.int_stack.pop())
+                else:
+                    raise Exception(
+                        f"{len(toks)}: Incorrect number of arguments for SUB!"
+                    )
+            elif toks[0].upper() == "DIV":
+                if len(toks) == 1:
+                    print("Div")
+                    self.int_stack.append(
+                        self.int_stack.pop(-2) // self.int_stack.pop()
+                    )
+                else:
+                    raise Exception(
+                        f"{len(toks)}: Incorrect number of arguments for DIV!"
+                    )
+            elif toks[0].upper() == "MUL":
+                if len(toks) == 1:
+                    print("Mul")
+                    self.int_stack.append(self.int_stack.pop(-2) * self.int_stack.pop())
+                else:
+                    raise Exception(
+                        f"{len(toks)}: Incorrect number of arguments for MUL!"
+                    )
+            elif toks[0].upper() == "MOD":
+                if len(toks) == 1:
+                    print("Mod")
+                    self.int_stack.append(self.int_stack.pop(-2) % self.int_stack.pop())
+                else:
+                    raise Exception(
+                        f"{len(toks)}: Incorrect number of arguments for MOD!"
+                    )
+            elif toks[0].upper() == "CMP":
+                if len(toks) == 1:
+                    print("Cmp")
+                    self.int_stack.append(
+                        int(self.int_stack.pop(-2) == self.int_stack.pop())
+                    )
+                else:
+                    raise Exception(
+                        f"{len(toks)}: Incorrect number of arguments for CMP!"
+                    )
+            elif toks[0].upper() == "AND":
+                if len(toks) == 1:
+                    print("And")
+                    self.int_stack.append(
+                        int(self.int_stack.pop(-2) and self.int_stack.pop())
+                    )
+                else:
+                    raise Exception(
+                        f"{len(toks)}: Incorrect number of arguments for AND!"
+                    )
+            elif toks[0].upper() == "OR":
+                if len(toks) == 1:
+                    print("Or")
+                    self.int_stack.append(
+                        int(self.int_stack.pop(-2) or self.int_stack.pop())
+                    )
+                else:
+                    raise Exception(
+                        f"{len(toks)}: Incorrect number of arguments for OR!"
+                    )
             elif toks[0].upper() == "BEGIN":
                 if len(toks) == 2:
                     name = toks[1]
@@ -52,7 +158,9 @@ class Interpreter:
                     self.def_stack.append(name)
                     self.defs[name] = []
                 else:
-                    raise Exception("Incorrect number of arguments for BEGIN!")
+                    raise Exception(
+                        f"{len(toks)}: Incorrect number of arguments for BEGIN!"
+                    )
 
 
 def main():
